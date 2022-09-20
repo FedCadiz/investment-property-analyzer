c177f1524ed5e40a0be0561ef070ddb1fa717325
FedCadiz <97087009+FedCadiz@users.noreply.github.com>
2022-09-20 08:58:49 -0700

    Add files via upload

---
+++ analyzer.py
@@ -0,0 +1,123 @@
+from decimal import Decimal
+from re import sub
+from turtle import down
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+import time
+
+class Analyzer:
+    def __init__(self,cityState,price,downPayment,interestRate,units,length):
+        self.city = (cityState.split(","))[0]
+        self.state = (cityState.split(","))[1]
+        self.price = price
+        self.downPayent = downPayment
+        self.interestRate = interestRate
+        self.units = units
+        self.length = length
+        self.loan = int(float(price) - (float(downPayment)*.01))
+        self.homeInsurance = 0
+
+        self.rentPrice = 1500
+        self.totalmonthlyRent = self.rentPrice * int(self.units)
+
+    def calculate(self):
+        # self.interestRate = self.interestRate/12
+        # self.monthlyMortgage = int(self.loan*self.interestRate*((1+self.interestRate)**self.length)/(((1+self.interestRate)**self.length)-1))
+        # return self.monthlyMortgage
+
+        options = Options()
+        options.headless = True
+        PATH = "C:\Program Files (x86)\chromedriver.exe"
+        driver = webdriver.Chrome(PATH,options=options)
+        driver.get("https://www.rocketmortgage.com/learn/property-taxes-by-state")
+        try:
+            check = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "b-Content"))
+            )
+        except:
+            driver.quit()
+        rows = driver.find_elements("tag name","tbody")
+        index = -1
+        for row in rows:
+            columns = driver.find_elements("tag name","td")
+            for column in columns:
+                index += 1
+                if str(column.text).lower() == str(self.state).lower():
+                    self.propertyTax = (columns[index+1].text).replace("%","")
+                else:
+                    pass
+
+        driver.get("https://www.calculator.net/mortgage-calculator.html")
+
+        price = driver.find_element("id","chouseprice")
+        price.clear()
+        price.send_keys(self.price)
+
+        downPayment = driver.find_element("id","cdownpayment")
+        downPayment.clear()
+        downPayment.send_keys(self.downPayent)
+
+        length = driver.find_element("id","cloanterm")
+        length.clear()
+        length.send_keys(self.length)
+
+        interestRate = driver.find_element("id","cinterestrate")
+        interestRate.clear()
+        interestRate.send_keys(self.interestRate)
+
+        propertyTax = driver.find_element("id","cpropertytaxes")
+        propertyTax.clear()
+        propertyTax.send_keys(self.propertyTax)
+
+        homeInsurance = driver.find_element("id","chomeins")
+        homeInsurance.clear()
+        homeInsurance.send_keys(2000)
+
+        if self.downPayent < 20:
+            pmi = driver.find_element("id","cpmi")
+            pmi.clear()
+            pmi.send_keys(int(self.loan*.01))
+        else:
+            pass
+
+        otherCosts = driver.find_element("id","cothercost")
+        otherCosts.clear()
+        otherCosts.send_keys(int(.01*float(self.price)))
+        otherCosts.send_keys(Keys.RETURN)
+
+        self.monthlyPayment = driver.find_element("class name","h2result")
+        self.monthlyPayment = (str(self.monthlyPayment.text).split(" ")[4])
+        self.monthlyPayment = Decimal(sub(r'[^\d.]', '', self.monthlyPayment))
+        print(f"Monthly Payment: {self.monthlyPayment}")
+
+        principalBalance = driver.find_element("xpath", "//div[@id='camortizationschdis']/table/tbody/tr[2]/td[5]")
+        self.principalBalance = principalBalance.text
+        self.principalBalance = Decimal(sub(r'[^\d.]', '', self.principalBalance))
+
+        self.totalRevenue = self.principalBalance + Decimal(self.totalmonthlyRent * 12)
+        print(f"Total Revenue: {self.totalRevenue}")
+
+        self.netProfit = self.totalRevenue - self.monthlyPayment
+
+        self.totalReturn = self.netProfit / self.price
+        print(f"ROI: {format(self.totalReturn,'.2%')}")
+
+        if self.totalReturn >= .07:
+            print("This rental property has a good return on investment")
+        else:
+            print("The return on investment is too low")
+
+
+        driver.close()
+
+
+
+
+
+# test = Analyzer("Garden Grove,California",500000,10,5,2,30)
+# test.calculate()
---
+++ gui.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+from tkinter import CENTER, HORIZONTAL, Label, font
+from tkinter.tix import COLUMN
+from analyzer import Analyzer
+
+
+
+
+class MyGui:
+    font = ("Helvetica",20)
+
+    def show_down_payment(self,percentage = 0):
+        purchasePrice = self.lpspinbox.get()
+        percentage = float(percentage) * .01
+        downPayment = (float(purchasePrice) * percentage)
+        self.dplabel.config(text=("${:0,.2f}".format(downPayment)))
+    
+    def analyze(self):
+        # self.analyze = Analyzer(self.csentry.get(),int(self.lpspinbox.get()),int(self.scale.get()),int(self.irentry.get()),int(self.urspinbox.get()),int(self.mlspinbox.get()))
+        self.analyze = Analyzer("Garden Grove,California",500000,10,5,2,30)
+        self.analyze.calculate()
+        self.analyzeWindow = tk.Toplevel()
+        self.analyzeFrame = tk.Frame(self.analyzeWindow)
+        self.analyzeFrame.pack()
+        self.a_title = tk.Label(self.analyzeFrame,text="Analysis Results",font=("helvetica",25,"bold"))
+        self.a_title.grid(row=0,columnspan=3)
+
+        self.label = tk.Label(self.analyzeFrame,text=f"Monthly Payment: ${self.analyze.monthlyPayment}",font=(self.font))
+        self.label.grid(row=1,column=0)
+
+        self.label = tk.Label(self.analyzeFrame,text=f"Gross Annual Rent: ${self.analyze.totalRevenue}",font=(self.font))
+        self.label.grid(row=1,column=1)
+
+        self.label = tk.Label(self.analyzeFrame,text=f"ROI: {format(self.analyze.totalReturn,'.2%')}",font=(self.font))
+        self.label.grid(row=1,column=2)
+        
+        
+
+        
+
+
+    def __init__(self,master):
+        windowFrame = tk.Frame(master)
+        windowFrame.pack()
+        
+        self.title = tk.Label(windowFrame,text="Investment Property Analyzer",font=("helvetica",25,"bold"))
+        self.title.pack(pady=(10,30))
+
+        self.label = tk.Label(windowFrame,text="City,State",font=(self.font))
+        self.label.pack()
+
+        self.csentry = tk.Entry(windowFrame,font=self.font)
+        self.csentry.pack()
+
+        self.label = tk.Label(windowFrame,text="Listing Price",font=(self.font))
+        self.label.pack(pady=(10,0))
+
+        self.lpspinbox = tk.Spinbox(windowFrame,from_=0, to=1000000,increment=1000,font=(self.font),justify=CENTER,command=self.show_down_payment)
+        self.lpspinbox.pack()
+
+
+        self.label = tk.Label(windowFrame, text="Down Payment (%)",font=(self.font))
+        self.label.pack(pady=(10,0))
+    
+        self.scale = tk.Scale(windowFrame,from_= 0, to=100,orient=HORIZONTAL,font=(self.font),length=200,command=self.show_down_payment)
+        self.scale.pack()
+
+        self.dplabel = tk.Label(windowFrame,text=" ",font=("Helvetica", 15))
+        self.dplabel.pack()
+
+        self.label = tk.Label(windowFrame,text="Interest Rate (%)",font=(self.font))
+        self.label.pack(pady=(10,0))
+
+        self.irentry = tk.Entry(windowFrame,font=self.font,width=5,justify=CENTER)
+        self.irentry.pack()
+
+        self.label = tk.Label(windowFrame,text="Mortgage Length",font=self.font)
+        self.label.pack(pady=(10,0))
+
+        self.mlspinbox = tk.Spinbox(windowFrame,from_=15,to=30,increment=15,font=self.font,justify=CENTER)
+        self.mlspinbox.pack()
+
+        self.label = tk.Label(windowFrame,text="Units Rented",font=(self.font))
+        self.label.pack(pady=(10,0))
+
+        self.urspinbox = tk.Spinbox(windowFrame,from_=0,to=4,increment=1,font=(self.font),justify=CENTER,width=8)
+        self.urspinbox.pack()
+
+        self.button = tk.Button(windowFrame,text="Analyze",font=("helvetica",20,"bold"),command=self.analyze)
+        self.button.pack(pady=(10))
+
+def run():
+    root = tk.Tk()
+    app = MyGui(root)
+    root.mainloop()
+
+run()
