6bf7211d26bc236a39e2147b04dda114f949bbfc
FedCadiz <fedcadiz@gmail.com>
2022-09-24 17:50:40 -0700

    removed unused modules

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,7 @@
 from decimal import Decimal
 from re import sub
-from turtle import down
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
 
