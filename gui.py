cd464546c111e038df821db0d8deed582d885abb
FedCadiz <fedcadiz@gmail.com>
2022-09-28 11:09:30 -0700

    improved loading screen and cleaned up analysis results window

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
+from cgitb import text
 import tkinter as tk
 from tkinter import CENTER, HORIZONTAL
 from analyzer import Analyzer
 import threading
+import time
 
 
 
@@ -14,32 +16,75 @@ class MyGui:
         percentage = float(percentage) * .01
         downPayment = (float(purchasePrice) * percentage)
         self.dplabel.config(text=("${:0,.2f}".format(downPayment)))
+        global dollardownpayment
+        dollardownpayment = ("${:0,.2f}".format(downPayment))
     
     def analyze(self):
         # self.analyze = Analyzer("Garden Grove,California",500000,10,5,2,30)
         self.analyze = Analyzer(self.csentry.get(),int(self.lpspinbox.get()),int(self.scale.get()),int(self.irentry.get()),int(self.urspinbox.get()),int(self.mlspinbox.get()))
-
+        self.loading = True
         def analyze_window():     
             self.analyzeWindow = tk.Toplevel()
             self.analyzeFrame = tk.Frame(self.analyzeWindow)
             self.analyzeFrame.pack()
             self.a_title = tk.Label(self.analyzeFrame,text="Analysis Results",font=("helvetica",25,"bold"))
-            self.a_title.grid(row=0,columnspan=3)
+            self.a_title.grid(row=0,columnspan=3,padx=500)
 
-            self.monthly_label = tk.Label(self.analyzeFrame,text="-",font=(self.font))
-            self.monthly_label.grid(row=1,column=0)
+            self.purchase_price = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.purchase_price.grid(row=1,column=0,padx=20,pady=10)
 
-            self.gar_label = tk.Label(self.analyzeFrame,text="Loading",font=(self.font))
-            self.gar_label.grid(row=1,column=1,padx=25)
+            self.down_payment = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.down_payment.grid(row=1,column=1,padx=20)
 
-            self.roi_label = tk.Label(self.analyzeFrame,text="-",font=(self.font))
-            self.roi_label.grid(row=1,column=2)
+            self.interest_rate = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.interest_rate.grid(row=1,column=2,padx=20)
+
+            self.mortgage_length = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.mortgage_length.grid(row=2,column=0,pady=10)
+            
+            self.monthly_label = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.monthly_label.grid(row=2,column=1)
+
+            self.city_state = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.city_state.grid(row=4,column=1)
+
+            self.units_rented = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.units_rented.grid(row=2,column=2)
+
+            self.gar_label = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.gar_label.grid(row=3,column=1,)
+
+            self.roi_label = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.roi_label.grid(row=3,column=2)
+
+            self.net_profit = tk.Label(self.analyzeFrame,text=" ",font=(self.font))
+            self.net_profit.grid(row=3,column=0,pady=10)
+
+            while self.loading == True:
+                self.monthly_label.config(text="Loading")
+                time.sleep(.5)
+                self.monthly_label.config(text="Loading.")
+                time.sleep(.5)
+                self.monthly_label.config(text="Loading..")
+                time.sleep(.5)
+                self.monthly_label.config(text="Loading...")
+                time.sleep(.5)
 
         def execute_analyze():
             self.analyze.calculate()
-            self.monthly_label.config(text=f"Monthly Payment: ${self.analyze.monthlyPayment}")
-            self.gar_label.config(text=f"Gross Annual Rent: ${self.analyze.totalRevenue}")
-            self.roi_label.config(text=f"ROI: {format(self.analyze.totalReturn,'.2%')}")
+            self.loading = False
+            time.sleep(2)
+            self.monthly_label.config(text=f"Monthly Payment: {self.analyze.monthlyPayment}")
+            self.gar_label.config(text=f"Gross Annual Rent: {self.analyze.totalRevenue}")
+            self.net_profit.config(text=f"Net Profit: {self.analyze.netProfit}")
+            self.roi_label.config(text=f"ROI: {format(self.analyze.roi,'.2%')}")
+            self.purchase_price.config(text=f"Purchase Price: {self.analyze.price}")
+            self.down_payment.config(text=f"Down Payment: {self.analyze.downPayment}% | {dollardownpayment}")
+            self.interest_rate.config(text=f"Interest Rate: {self.analyze.interestRate}%")
+            self.mortgage_length.config(text=f"Mortgage Length: {self.analyze.length} years")
+            self.city_state.config(text=f"Location: {self.analyze.city}, {self.analyze.state}")
+            self.units_rented.config(text=f"Units Rented: {self.analyze.units}")
+            
         t1 = threading.Thread(target=analyze_window)
         t2 = threading.Thread(target=execute_analyze)
         t1.start()
